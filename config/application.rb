595df9f42b5658b4cabc8bb2efdd2fd434a3b08d
wind-up-bird <nagi.ooooo.1@gmail.com>
2021-08-15 18:35:31 +0900

    Add ja

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module BooksApp
   class Application < Rails::Application
+    #config.i18n.default_locale = :en
+    config.i18n.default_locale = :ja
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
